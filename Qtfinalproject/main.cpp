0c37ff2b01ee985a9b7177763ad43fd77a7ac650
CHENGJUI-TSAI <41243252@nfu.edu.tw>
2025-01-12 13:23:11 +0800

    Add files via upload

---
+++ Qtfinalproject/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "Qtproject.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Qtproject w;
+    w.show(); // 僅顯示主視窗
+    return a.exec();
+}
